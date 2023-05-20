5dbc3aacc1ab6f73cb39a095ad349632f4ec3cc9
andradebru <brunansandrade@gmail.com>
2023-05-19 21:46:00 -0300

    reapeting myself a lot...

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const translation = {
     en: {
         linkHome: "home //",
-        linkAbout: "about her //",
+        linkAbout: "about //",
+        linkContact: "contact //",
+        socialMedia: "In my spare time..."
+
     },
     pt: {
         linkHome: "início //",
         linkAbout: "quem é //",
+        linkContact: "contato //",
+        socialMedia: "In my spare time..."
+
     }
   };
   
 let currentLanguage = "en";
 
 function switchLanguage() {
-currentLanguage = currentLanguage === "en" ? "pt" : "en";
-updateTexts();
+    currentLanguage = currentLanguage === "en" ? "pt" : "en";
+    updateTexts();
 }
 
 function updateTexts() {
-const content = translation[currentLanguage];
-document.getElementById("linkHome").textContent = content.linkHome;
-document.getElementById("linkAbout").textContent = content.linkAbout;
+    const content = translation[currentLanguage];
+    document.getElementById("linkHome").textContent = content.linkHome;
+    document.getElementById("linkAbout").textContent = content.linkAbout;
+    document.getElementById("linkAbout").textContent = content.linkAbout;
+    document.getElementById("linkContact").textContent = content.linkContact;
+    document.getElementById("socialMedia").textContent = content.socialMedia;
 }
 
 document.getElementById("languageSwitch").addEventListener("click", switchLanguage);
